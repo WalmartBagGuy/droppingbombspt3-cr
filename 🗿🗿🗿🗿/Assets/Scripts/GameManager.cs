f6376f02d1719f7b6a010697d432ac3b083289ca
BillySwCodeNinjas <129233042+WalmartBagGuy@users.noreply.github.com>
2023-11-21 18:23:05 -0600

    46

--- 🗿🗿🗿🗿/Assets/Scripts/GameManager.cs
+++ 🗿🗿🗿🗿/Assets/Scripts/GameManager.cs
@@ -5,22 +5,52 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     private Spawner spawner;
+    public GameObject title;
+    private Vector2 screenBounds;
+    public GameObject playerPrefab
+    private GameObject player;
+    private bool gameStarted = false;
         
         void Awake()
         {
             spawner = GameObject.Find("Spawner").GetComponent<Spawner>();
+            screenBounds = camera.main.ScreenToWorldPoint(new Vector3 (Screen.width, Screen.height, Camera.main.transform.position.z));
+            player = playerPrefab;
         }
     void Start()
     {
         spawner.active = false;
+        title.SetActive(true);
     }
 
     // Update is called once per frame
     void Update()
     {
+     if(!gameStarted) 
+     { 
        if (Input.anyKeyDown)
        {
-              spawner.active = true;
+              //spawner.active = true;
+              //title.SetActive(false);
+              ResetGame();
        }
+     }
+    }
+    var nextBomb = GameObject.FindGameObjectsWithTag("Bomb");
+
+    foreach (GameObject bombObgect in nextBomb)
+    {
+        if (bombObgect.transform.position.y < (-screenBounds.y) - 12)
+        {
+            Destroy(bombObgect);
+        }
+    }
+    void ResetGame()
+    {
+        spawner.active = true;
+        title.SetActive(false);
+        player = Instantiate(playerPrefab, new Vector3(0,0,0), playerPrefab.transform.rotation);
+        gameStarted = true;
+
     }
 }
